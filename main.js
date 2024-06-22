4e816602bc5b492162b77176c41bfd9581bb7a7a
pradumancodes <praduman0571@gmail.com>
2024-06-22 23:29:42 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    strings: ["Frontend Developer", "Web Developer", "Full Stack Developer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true,
+});
