1093a5bfbea4981953f8550eb5dd548a8f416337
Praduman Sharma <154724772+pradumancodes@users.noreply.github.com>
2024-09-01 18:52:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed= new Typed(".text", {
-    strings: ["Frontend Developer", "Web Developer", "Full Stack Developer"],
+    strings: ["Software Developer", "Web Developer", "Full Stack Developer"],
     typeSpeed: 50,
     backSpeed: 50,
     backDelay: 1000,
